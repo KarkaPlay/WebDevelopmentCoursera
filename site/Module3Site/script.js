2703aafba8fea799ea16d95e9cf4d45bbc1fd56b
Sergei Karpushkin <c1e2r3g@gmail.com>
2022-02-20 01:04:11 +0300

    Update script.js

--- site/Module3Site/script.js
+++ site/Module3Site/script.js
@@ -1,4 +1,14 @@
-const a = 5;
-const b = 4;
+const message = "in global";
+console.log("global message: " + message)
 
-alert(a+b);
+const a = function () {
+    const message = "inside a";
+    console.log("a message: " + message)
+    b()
+}
+
+function b() {
+    console.log("b message: " + message)
+}
+
+a();
